b774d65d0974db880a5fc169dbb370bc1acecb1b
Diego Jv <djaimes10@gmail.com>
2025-07-28 17:41:50 -0600

    feat: Define Firebase configuration using environment variables for better security

--- src/lib/firebase-config-client.js
+++ src/lib/firebase-config-client.js
@@ -3,5 +3,13 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
 
 
+const firebaseConfig = {
+  apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
+  authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
+  appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
+  // Los otros campos no son necesarios para Auth
+};
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
