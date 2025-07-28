100ec403d0ec91f3c7ef45655c8a9b7e43c898dd
Diego Jv <djaimes10@gmail.com>
2025-07-28 17:34:49 -0600

    feat: Remove hardcoded Firebase configuration from client code

--- src/lib/firebase-config-client.js
+++ src/lib/firebase-config-client.js
@@ -3,15 +3,5 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
 
 
- const firebaseConfig = {
-  apiKey: "AIzaSyCDZNK9I3vvOWHzIElLf0NrlV0Q2Pq_VR4",
-  authDomain: "gyd-cms.firebaseapp.com",
-  projectId: "gyd-cms",
-  storageBucket: "gyd-cms.firebasestorage.app",
-  messagingSenderId: "1015954699793",
-  appId: "1:1015954699793:web:08f330853d12c1fdfae3b0",
-  measurementId: "G-QGPVQ3LLVE"
-};
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
