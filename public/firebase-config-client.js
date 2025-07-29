e1de0628470a544fe5320b0e655e19ad37d6cbaf
Diego Jv <djaimes10@gmail.com>
2025-07-29 17:29:51 -0600

    fix: Update Firebase configuration to use public environment variable names

--- public/firebase-config-client.js
+++ public/firebase-config-client.js
@@ -4,10 +4,10 @@ import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 
 
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.FIREBASE_PROJECT_ID,
-  appId: import.meta.env.FIREBASE_APP_ID,
+  apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
+  authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
+  appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
   // Los otros campos no son necesarios para Auth
 };
 
