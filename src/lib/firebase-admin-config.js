9cc9bb5fbd74ce315268c582f7af1cc02e8346ca
Diego Jv <djaimes10@gmail.com>
2025-08-05 12:46:37 -0600

    fix fix of storage name

--- src/lib/firebase-admin-config.js
+++ src/lib/firebase-admin-config.js
@@ -16,7 +16,7 @@ function initializeFirebaseAdmin() {
     if (admin.apps.length === 0) {
       admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
-        storageBucket: serviceAccount.projectId,
+        storageBucket: `${serviceAccount.projectId}.appspot.com`,
       });
     }
 
